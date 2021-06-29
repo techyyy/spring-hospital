bc522511c647d84abeda8f9338df22d1ea535046
Yurii Horobets <yurchik123456@gmail.com>
2021-06-29 13:31:11 +0300

    v 1.1 Small changes in error handling.

--- src/main/java/com/myhospital/config/WebMVCConfig.java
+++ src/main/java/com/myhospital/config/WebMVCConfig.java
@@ -1,6 +1,7 @@
 package com.myhospital.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
